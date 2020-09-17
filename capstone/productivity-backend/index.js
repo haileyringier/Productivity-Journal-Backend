d333650725235464a2ac39580f89b885cdf8d2c7
haileyringier <haileyringier@outlook.com>
2020-09-17 17:46:46 -0600

    adds seeds and get routes

--- capstone/productivity-backend/index.js
+++ capstone/productivity-backend/index.js
@@ -1,17 +1,39 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
- 
-app.get('/', function (request, response) {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
+const port = '3000'
+const database = require('./database')
+const bodyParser = require('body-parser')
+const bcyrpt = require('bcrypt')
+const saltRounds = 12
+const jwt = require('jsonwebtoken')
+
+app.use(cors())
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`)
 })
- 
 
-const PORT = '3000'
-app.listen(PORT, () => {[
-    console.log(`App running on port ${PORT}`)
-]})
+app.get('/habits', (request, response) => {
+  database('habit').select()
+    .then(habits => response.json({habits}))
+})
+app.get('/journal', (request, response) => {
+  database('journal').select()
+    .then(journal => response.json({journal}))
+})
+app.get('/goals', (request, response) => {
+  database('goal').select()
+    .then(goals => response.json({goals}))
+})
+app.get('/events', (request, response) => {
+  database('event').select()
+    .then(events => response.json({events}))
+})
 
-const postgres = require('postgres')
- 
-const sql = postgres({ ...options }) 
- 
+
+
+
